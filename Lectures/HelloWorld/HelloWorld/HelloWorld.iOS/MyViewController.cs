bd7ac6c2c78b49d978c215b4e224fec773ed6914
Hrafn Loftsson <hrafn@kolibri.is>
2016-11-28 10:58:58 +0000

    Adds a runnable HelloWorld app (blank screen)

---
+++ Lectures/HelloWorld/HelloWorld/HelloWorld.iOS/MyViewController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.iOS
+{
+    using UIKit;
+    public class MyViewController : UIViewController
+    {
+    }
+}
