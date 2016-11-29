b6b3a1dcb982a41f508cd380219217335e8dc450
Hrafn Loftsson <hrafn@kolibri.is>
2016-11-29 11:38:56 +0000

    Finishes the functionality of displaying the list of people

--- Lectures/HelloWorld/HelloWorld/HelloWorld.iOS/NameListController.cs
+++ Lectures/HelloWorld/HelloWorld/HelloWorld.iOS/NameListController.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace HelloWorld.iOS
 {
     using UIKit;
-    public class NameListController : UIViewController
+    public class NameListController : UITableViewController
     {
         private List<string> _nameList;
 
@@ -17,6 +17,9 @@ namespace HelloWorld.iOS
         public override void ViewDidLoad()
         {
             this.View.BackgroundColor = UIColor.White;
+            this.Title = "Name list";
+
+            this.TableView.Source = new NameListSource(this._nameList);
         }
     }
 }
---
+++ Lectures/HelloWorld/HelloWorld/HelloWorld.iOS/NameListSource.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.iOS
+{
+    using Foundation;
+
+    using UIKit;
+
+    public class NameListSource : UITableViewSource
+    {
+        public readonly NSString NameListCellId = new NSString("NameListCell");
+
+        private List<string> _nameList;
+
+        public NameListSource(List<string> nameList)
+        {
+            this._nameList = nameList;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell(this.NameListCellId);
+
+            if (cell == null)
+            {
+                cell = new UITableViewCell(UITableViewCellStyle.Default, (NSString)this.NameListCellId);
+            }
+
+            int row = indexPath.Row;
+            cell.TextLabel.Text = this._nameList[row];
+
+            return cell;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return this._nameList.Count;
+        }
+    }
+}
